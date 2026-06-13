269e4cdf618da91895cbd33c2d5e2ffaf3390cf8
YFMARCO-Dev <qahralandrwyd4@gmail.com>
2026-06-13 09:11:17 +0300

    chore(copyright): update copyright headers for 2026
    Updates the copyright year to 2026 for the LineageOS Project and adds
    attribution for YFMARCO-Dev.
    
    - Updates LineageOS copyright to 2026 in common.mk and releasetools.py
    - Adds YFMARCO-Dev copyright notice to both files

--- common.mk
+++ common.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2019-2025 The LineageOS Project
+# Copyright (C) 2026 The LineageOS Project
+# copyright (C) 2026 YFMARCO-Dev
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
